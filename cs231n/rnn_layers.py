3ebe1a1171c382a5f2fcdc26b8f38934b008a063
Santiago Castro <santi.1410@hotmail.com>
2017-03-20 20:13:27 -0300

    Implement rnn_step_forward

--- cs231n/rnn_layers.py
+++ cs231n/rnn_layers.py
@@ -32,7 +32,7 @@ def rnn_step_forward(x, prev_h, Wx, Wh, b):
   # hidden state and any values you need for the backward pass in the next_h   #
   # and cache variables respectively.                                          #
   ##############################################################################
-  pass
+  next_h = np.tanh(x.dot(Wx) + prev_h.dot(Wh) + b)
   ##############################################################################
   #                               END OF YOUR CODE                             #
   ##############################################################################
